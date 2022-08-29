9291c248d6884a20dfefc379d1da0dda4124e93f
NikolaySlabchenko <76903664+NikolaySlabchenko@users.noreply.github.com>
2022-08-29 07:13:31 +0300

    PIP8 исправил в модели Games

--- gamach_back/games/models.py
+++ gamach_back/games/models.py
@@ -11,9 +11,11 @@ class Games(models.Model):
         (1, 'Да'),
         (2, 'Нет')
     ]
-    requirements = models.PositiveSmallIntegerField(choices=PC_REQUIREMENTS, default=2, verbose_name="Системные требования")
+    requirements = models.PositiveSmallIntegerField(choices=PC_REQUIREMENTS, default=2, verbose_name="Системные "
+                                                                                                     "требования")
     title = models.CharField(blank=True, max_length=256, verbose_name="Название игры")
     release_date = models.DateField(blank=True, verbose_name="Дата релиза")
     description = models.CharField(blank=True, max_length=1000, verbose_name="Описание")
     picture = models.ImageField(upload_to='pictures/%Y/%m/%d/', verbose_name="Картинка")
-    torrent_available = models.PositiveSmallIntegerField(choices=Tor_Available, default=1, verbose_name="Альтернативно бесплатно?")
+    torrent_available = models.PositiveSmallIntegerField(choices=Tor_Available, default=1, verbose_name="Альтернативно"
+                                                                                                        " бесплатно?")
