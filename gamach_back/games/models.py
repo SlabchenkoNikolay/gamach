39b5c968e811d562a566fc7d854a1c1a8d894c31
SlabchenkoNikolay <96453848+SlabchenkoNikolay@users.noreply.github.com>
2022-08-29 06:27:23 +0300

    Add files via upload

---
+++ gamach_back/games/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Games(models.Model):
+    PC_REQUIREMENTS = [
+        (1, 'Суперкомпьютер'),
+        (2, 'Уверенный середнячок'),
+        (3, 'Умная колонка')
+    ]
+    Tor_Available = [
+        (1, 'Да'),
+        (2, 'Нет')
+    ]
+    requirements = models.PositiveSmallIntegerField(choices=PC_REQUIREMENTS, default=2, verbose_name="Системные требования")
+    title = models.CharField(blank=True, max_length=256, verbose_name="Название игры")
+    release_date = models.DateField(blank=True, verbose_name="Дата релиза")
+    description = models.CharField(blank=True, max_length=1000, verbose_name="Описание")
+    picture = models.ImageField(upload_to='pictures/%Y/%m/%d/', verbose_name="Картинка")
+    torrent_available = models.PositiveSmallIntegerField(choices=Tor_Available, default=1, verbose_name="Альтернативно бесплатно?")
+
+    def __str__(self):
+        return self.title
